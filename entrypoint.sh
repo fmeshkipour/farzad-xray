5818f61629d7fdfb4e857f0ec97e1bb1bf7f1e89
fmeshkipour <123179648+fmeshkipour@users.noreply.github.com>
2023-02-13 12:59:57 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ sed -i "s/uuid/$uuid/g" /etc/nginx/nginx.conf
 cat config.json | base64 > config
 rm -f config.json
 
-# argo与加密方案出自fscarmen
+# argo fscarmen
 rm -f cloudflared-linux-amd64*
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 chmod +x cloudflared-linux-amd64
@@ -47,80 +47,68 @@ Argo_xray_trojan="trojan://${uuid}@${ARGO}:443?security=tls&type=ws&host=${ARGO}
 
 cat > log << EOF
 ****************************************************************
-相关教程解读，请关注：甬哥侃侃侃
-视频教程：https://www.youtube.com/@ygkkk
-博客地址：https://ygkkk.blogspot.com
-================================================================
-当前已安装的Xray正式版本：$xver
-当前网络的IP：$v4
-IP归属地区：$v4l
-================================================================
-注意：重构或重启当前平台，Argo服务器地址将重置更新
-Cloudflared Argo 隧道模式Xray五协议配置如下：
-================================================================
-----------------------------------------------------------------
-1：Vmess+ws+tls配置明文如下，相关参数可复制到客户端
-Argo服务器临时地址（可更改为CDN自选IP）：$ARGO
-https端口：可选443、2053、2083、2087、2096、8443，tls必须开启
-http端口：可选80、8080、8880、2052、2082、2086、2095，tls必须关闭
-uuid：$uuid
-传输协议：ws
-host/sni：$ARGO
-path路径：/$uuid-vm
-
-分享链接如下（默认443端口、tls开启，服务器地址可更改为自选IP）
+Currently installed Xray official version: $xver
+Current network IP：$v4
+IP：$v4l
+================================================== ==============
+Note: Restart the current platform, Argo server address will be updated
+Cloudflared Argo Tunnel mode Xray
+================================================== ==============
+----------------------------------------------------- --------------
+1：Vmess+ws+tls configuration，related parameters can be copied to the client
+Argo server temporary address (can be changed to CDN IP): $ARGO
+https port: optional 443、2053、2083、2087、2096、8443、tls must be opened
+http port: optional 80、8080、8880、2052、2082、2086、2095、tls must be closed
+uuid: $uuid
+Transmission protocol: ws
+host/sni: $ARGO
+path: /$uuid-vm
+(default 443 port, tls open, server address can be changed to self-selected IP)
 ${Argo_xray_vmess}
-
-----------------------------------------------------------------
-2：Vless+ws+tls配置明文如下，相关参数可复制到客户端
-Argo服务器临时地址（可更改为CDN自选IP）：$ARGO
-https端口：可选443、2053、2083、2087、2096、8443，tls必须开启
-http端口：可选80、8080、8880、2052、2082、2086、2095，tls必须关闭
-uuid：$uuid
-传输协议：ws
-host/sni：$ARGO
-path路径：/$uuid-vl
-
-分享链接如下（默认443端口、tls开启，服务器地址可更改为自选IP）
+----------------------------------------------------- --------------
+2：Vless+ws+tls configuration，Relevant parameters can be copied to the client
+Argo server temporary address (can be changed to CDN IP): $ARGO
+https port: optional 443、2053、2083、2087、2096、8443、tls must be opened
+http port: optional 80、8080、8880、2052、2082、2086、2095、tls must be closed
+uuid: $uuid
+Transmission protocol: ws
+host/sni: $ARGO
+path: /$uuid-vl
+(default 443 port, tls open, server address can be changed to self-selected IP)
 ${Argo_xray_vless}
-
-----------------------------------------------------------------
-3：Trojan+ws+tls配置明文如下，相关参数可复制到客户端
-Argo服务器临时地址（可更改为CDN自选IP）：$ARGO
-https端口：可选443、2053、2083、2087、2096、8443，tls必须开启
-http端口：可选80、8080、8880、2052、2082、2086、2095，tls必须关闭
-密码：$uuid
-传输协议：ws
-host/sni：$ARGO
-path路径：/$uuid-tr
-
-分享链接如下（默认443端口、tls开启，服务器地址可更改为自选IP）
+----------------------------------------------------- --------------
+3：Trojan+ws+tls configuration，Relevant parameters can be copied to the client
+Argo server temporary address (can be changed to CD IP): $ARGO
+https port: optional 443、2053、2083、2087、2096、8443、tls must be opened
+http port: optional 80、8080、8880、2052、2082、2086、2095、tls must be closed
+Password: $uuid
+Transmission protocol: ws
+host/sni: $ARGO
+path: /$uuid-tr
+(default 443 port, tls open, server address can be changed to self-selected IP)
 ${Argo_xray_trojan}
-
-----------------------------------------------------------------
-4：Shadowsocks+ws+tls配置明文如下，相关参数可复制到客户端
-Argo服务器临时地址（可更改为CDN自选IP）：$ARGO
-https端口：可选443、2053、2083、2087、2096、8443，tls必须开启
-http端口：可选80、8080、8880、2052、2082、2086、2095，tls必须关闭
-密码：$uuid
-加密方式：chacha20-ietf-poly1305
-传输协议：ws
-host/sni：$ARGO
-path路径：/$uuid-ss
-
-----------------------------------------------------------------
-5：Socks+ws+tls配置明文如下，相关参数可复制到客户端
-Argo服务器临时地址（可更改为CDN自选IP）：$ARGO
-https端口：可选443、2053、2083、2087、2096、8443，tls必须开启
-http端口：可选80、8080、8880、2052、2082、2086、2095，tls必须关闭
-用户名：$uuid
-密码：$uuid
-传输协议：ws
-host/sni：$ARGO
-path路径：/$uuid-so
-
-----------------------------------------------------------------
-如当前环境支持shell，输入cat log查看当前配置信息
+----------------------------------------------------- --------------
+4：Shadowsocks+ws+tls configuration，Relevant parameters can be copied to the client
+Argo server temporary address (can be changed to CDN IP): $ARGO
+https port: optional 443、2053、2083、2087、2096、8443、tls must be opened
+http port: optional 80、8080、8880、2052、2082、2086、2095、tls must be closed
+Password: $uuid
+Encryption method: chacha20-ietf-poly1305
+Transmission protocol: ws
+host/sni: $ARGO
+path: /$uuid-ss
+----------------------------------------------------- --------------
+5：Socks+ws+tls configuration，Relevant parameters can be copied to the client
+Argo server temporary address (can be changed to CDN IP): $ARGO
+https port: optional 443、2053、2083、2087、2096、8443、tls must be opened
+http port: optional 80、8080、8880、2052、2082、2086、2095、tls must be closed
+User name: $uuid
+Password: $uuid
+Transmission protocol: ws
+host/sni: $ARGO
+path: /$uuid-so
+----------------------------------------------------- --------------
+If the current environment supports shell, enter cat log to view the current configuration information
 ****************************************************************
 EOF
  
